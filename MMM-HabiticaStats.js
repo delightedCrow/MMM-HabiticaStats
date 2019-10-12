09bcd0420797321aca1bff924b7be70704205d2a
delightedCrow <delightedCrow@pm.me>
2019-10-12 11:22:38 -0700

    initial commit
    yay! a wild prototype appears!

---
+++ MMM-HabiticaStats.js
@@ -0,0 +1,55 @@
+/* Magic Mirror
+ * Module: MMM-HabiticaStats
+ *
+ * By JSC (@delightedCrow)
+ * MIT Licensed.
+ */
+Module.register("MMM-HabiticaStats", {
+	defaults: {
+    userID: null,
+    APIToken: null
+	},
+
+	getStyles: function() {
+		return [
+			this.file("assets/MMM-HabiticaStats.css"),
+			"font-awesome5.css"
+		];
+	},
+
+	start: function() {
+		Log.info("Starting module: " + this.name);
+    this.getPartyData();
+	},
+
+  getTemplate: function() {
+		return "MMM-HabiticaStats.njk"
+	},
+
+	getTemplateData: function() {
+    return {};
+	},
+
+  getPartyData: function() {
+    let url = 'https://habitica.com/api/v3/groups/party';
+    let req = new XMLHttpRequest();
+    let mod = this;
+
+    req.addEventListener("load", function() {
+      Log.info(JSON.parse(this.responseText));
+      if (this.status == 200) {
+        Log.info("DID IT!");
+      }
+    });
+
+    req.addEventListener("error", function() {
+      // most likely an internet connection issue
+      Log.error("Could not connect to the Habitica server.");
+    });
+
+    req.open("GET", url);
+    req.setRequestHeader('x-api-user', this.config.userID);
+    req.setRequestHeader('x-api-key', this.config.APIToken);
+    req.send();
+  }
+});
