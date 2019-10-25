6b7d0864e8d434c3e3bea86222272972259702f5
delightedCrow <delightedCrow@pm.me>
2019-10-24 17:21:41 -0700

    Getting daily damage calculations working
    Aaaand it finally works!

--- MMM-HabiticaStats.js
+++ MMM-HabiticaStats.js
@@ -10,11 +10,8 @@ Module.register("MMM-HabiticaStats", {
     APIToken: null
 	},
     userData: {},
-		userTasks: {},
-		HABITICA_MAGIC: {
-			"taskCapMin": -47.27,
-			"taskCapMax": 21.27
-		},
+		questContent: {},
+		gearContent: {},
 
 	getStyles: function() {
 		return [
@@ -26,10 +23,8 @@ Module.register("MMM-HabiticaStats", {
 	start: function() {
 		Log.info("Starting module: " + this.name);
     // get content data first
-		// https://habitica.com/api/v3/content
+		this.getContentData();
     this.getAuthenticatedUserData();
-		this.getPartyData();
-		// this.getAuthenticatedUserTasks();
 	},
 
   getTemplate: function() {
@@ -37,7 +32,6 @@ Module.register("MMM-HabiticaStats", {
 	},
 
 	getTemplateData: function() {
-    Log.info("Refreshing template: ", this.userProfile);
     return this.userData;
 	},
 
@@ -64,9 +58,10 @@ Module.register("MMM-HabiticaStats", {
               userData.mana = Math.round(data.stats.mp);
               userData.max_mana = Math.round(data.stats.maxMP);
 
-							userData.constitution = data.stats.con + data.stats.buffs.con;
 							userData.quest = data.party.quest;
 							userData.stealth = data.stats.buffs.stealth;
+							userData.armorBuffs = mod.calculateArmorBuffs(userData.classname, data.items.gear.equipped);
+							userData.constitution = data.stats.con + data.stats.buffs.con + userData.armorBuffs.con + Math.floor(userData.level/2);
               mod.userData = userData;
               mod.updateDom();
 							mod.getAuthenticatedUserTasks();
@@ -92,8 +87,19 @@ Module.register("MMM-HabiticaStats", {
 				if (this.status == 200) {
 						let data = JSON.parse(this.responseText).data;
 						Log.info("AuthenticatedUserTasks: ", data);
-						mod.userTasks = mod.calculateDamage(mod.userData.constitution, mod.userData.stealth, data, mod.userData.quest);
-						Log.info("tasks: ", mod.userTasks);
+
+						var bossStrength = null;
+						// if user has a quest key they are on a quest
+						if (mod.userData.quest.key) {
+							let quest = mod.questContent[mod.userData.quest.key];
+							// if the quest has a boss it's a boss quest, otherwise it's a collection quest
+							if (quest.boss) {
+								bossStrength = quest.boss.str;
+							}
+						}
+
+						mod.userData.tasks = mod.calculateDamage(mod.userData.constitution, mod.userData.stealth, data, bossStrength);
+						// Log.info("tasks: ", mod.userData.tasks);
 
 						mod.updateDom();
 				}
@@ -109,7 +115,7 @@ Module.register("MMM-HabiticaStats", {
 		req.send();
 	},
 
-	calculateDamage: function(constitution, stealth, taskList, quest) {
+	calculateDamage: function(constitution, stealth, taskList, bossStrength) {
 		// takes a list of dailies, todos, habits, etc.
 		// returns the following data object:
 		var data = {};
@@ -130,7 +136,6 @@ Module.register("MMM-HabiticaStats", {
 			switch (task.type) {
 				case "daily":
 					if (task.isDue && !task.completed) {
-						Log.info("Daily: ", task.text);
 						// thieves can cast stealth to avoid a certain number of dailies
 						if (stealthRemaining > 0) { // avoided!
 							stealthRemaining --;
@@ -155,26 +160,51 @@ Module.register("MMM-HabiticaStats", {
 							}
 
 							var damage = taskDamage * conBonus * task.priority * 2;
-							data.damageToSelf += Math.round(damage * 10) / 10; // round damage to nearest tenth
+							data.damageToSelf += Math.round(damage * 10) / 10; // round damage to nearest tenth because game does
+							let selfDamageNoBoss = damage;
 
 							// if we have a quest and a boss we can calculate the damage to party from this daily
-							if (quest.content && quest.content.boss) {
+							if (bossStrength !== null) {
 								var bossDamage = (task.priority < 1) ? (taskDamage * task.priority) : taskDamage;
-								bossDamage *= quest.content.boss.str;
+								bossDamage *= bossStrength;
 								data.damageToParty += bossDamage;
 								data.damageToSelf += bossDamage;
 							}
+							// Log.info("Daily: " + task.text + ", Damage to self: " + selfDamageNoBoss + ", Self+Boss: " + (selfDamageNoBoss + bossDamage) + ", To Party: " + bossDamage + " Checklist: " + task.checklist.length + " Con: " + constitution);
 						}
 					}
-				break;
+					break;
 				case "todo":
-				// count number of todo's due
-				// figure out what makes a task due
+					// We want all todos with a date that are due today (this should include overdue tasks)
+					if (task.date) {
+						Log.info("TODO: ", task);
+					}
 				break;
 			}
 		}
+		// formatting display - rounding up to be safe like Lady Alys does :) - see https://github.com/Alys/tools-for-habitrpg/blob/29710e0f99c9d706d6911f49d60bcceff2792768/habitrpg_user_data_display.html#L1952
+		data.damageToSelf = Math.ceil(data.damageToSelf * 10) / 10;
+		data.damageToParty = Math.ceil(data.damageToParty * 10) / 10;
 
 		return data;
+	},
+
+	calculateArmorBuffs: function(userClass, equippedGear) {
+		// get a list of the actual item things from Content
+		var armorBuffs = {str: 0, con: 0, int: 0, per: 0};
+		for (keyName in equippedGear) {
+
+			let armor = this.gearContent[equippedGear[keyName]];
+			for (stat in armorBuffs) {
+				armorBuffs[stat] += armor[stat];
+				// apply class bonus
+				if (userClass === armor.klass || userClass === armor.specialClass) {
+					armorBuffs[stat] += .5 * armor[stat];
+				}
+			}
+		}
+		Log.info("Armor buffs: ", armorBuffs);
+		return armorBuffs;
 	},
 
   getPartyData: function() {
@@ -186,6 +216,31 @@ Module.register("MMM-HabiticaStats", {
       if (this.status == 200) {
 				let data = JSON.parse(this.responseText);
         Log.info("Party Data: ", data);
+      }
+    });
+
+    req.addEventListener("error", function() {
+      // most likely an internet connection issue
+      Log.error("Could not connect to the Habitica server.");
+    });
+
+    req.open("GET", url);
+    req.setRequestHeader('x-api-user', this.config.userID);
+    req.setRequestHeader('x-api-key', this.config.APIToken);
+    req.send();
+  },
+
+	getContentData: function() {
+    let url = 'https://habitica.com/api/v3/content';
+    let req = new XMLHttpRequest();
+    let mod = this;
+
+    req.addEventListener("load", function() {
+      if (this.status == 200) {
+				let data = JSON.parse(this.responseText);
+				mod.questContent = data.data.quests;
+				mod.gearContent = data.data.gear.flat;
+        Log.info("Content data: ", mod.questContent);
       }
     });
 
